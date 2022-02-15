b203c32973310911ed75ec717de391e885c3957f
Roja Ennam <roennam@microsoft.com>
2022-02-14 17:00:07 -0800

    Initial commit

--- src/Microsoft.Identity.VersionMatchAnalyzer/Microsoft.Identity.VersionMatchAnalyzer/MicrosoftIdentityVersionMatchAnalyzerAnalyzer.cs
+++ src/Microsoft.Identity.VersionMatchAnalyzer/Microsoft.Identity.VersionMatchAnalyzer/MicrosoftIdentityVersionMatchAnalyzerAnalyzer.cs
@@ -16,6 +16,34 @@ namespace Microsoft.Identity.VersionMatchAnalyzer
     public class MicrosoftIdentityVersionMatchAnalyzerAnalyzer : DiagnosticAnalyzer
     {
         public const string DiagnosticId = "MicrosoftIdentityVersionMatchAnalyzer";
+        public readonly List<string> WilsonAssemblyNames = new List<string>()
+        {
+            "Microsoft.IdentityModel.JsonWebTokens",
+            "Microsoft.IdentityModel.KeyVaultExtensions",
+            "Microsoft.IdentityModel.Logging",
+            "Microsoft.IdentityModel.ManagedKeyVaultSecurityKey",
+            "Microsoft.IdentityModel.Protocols.OpenIdConnect",
+            "Microsoft.IdentityModel.Protocols.SignedHttpRequest",
+            "Microsoft.IdentityModel.Protocols.WsFederation",
+            "Microsoft.IdentityModel.Protocols",
+            "Microsoft.IdentityModel.TestExtensions",
+            "Microsoft.IdentityModel.Tokens.Saml",
+            "Microsoft.IdentityModel.Tokens",
+            "Microsoft.IdentityModel.Validators",
+            "Microsoft.IdentityModel.Xml",
+            "System.IdentityModel.Tokens.Jwt"
+            // TODO: add older wilson assembly names as well
+        };
+        public readonly List<string> SalAssemblyNames = new List<string>()
+        {
+            "Microsoft.IdentityModel.S2S",
+            "Microsoft.IdentityModel.S2S.Configuration",
+            "Microsoft.IdentityModel.S2S.Extensions.AspNetCore",
+            "Microsoft.IdentityModel.S2S.Extensions.Owin",
+            "Microsoft.IdentityModel.S2S.Tokens.SealExtensions",
+            "Microsoft.IdentityModel.S2S.SubstrateExtensions",
+            "Microsoft.IdentityModel.S2S.Tokens"
+        };
 
         private static DiagnosticDescriptor AssembliesVersionMismatch { get; } =
        new DiagnosticDescriptor(
@@ -39,41 +67,61 @@ namespace Microsoft.Identity.VersionMatchAnalyzer
                 //Debugger.Launch();
                 var compilation = ctxt.Compilation;
                 var assemblies = compilation.ReferencedAssemblyNames;
-                int count = assemblies.Count();
-                var wilsonAssemblies = new List<AssemblyIdentity>();
+                var foundWilsonAssemblies = new List<AssemblyIdentity>();
+                var foundSalAssemblies = new List<AssemblyIdentity>();
+                var wilsonMessage = new StringBuilder();
+                var salMessage = new StringBuilder();
 
                 foreach (var assembly in assemblies)
                 {
-                    if (assembly.Name.StartsWith("Microsoft.IdentityModel.") || assembly.Name.StartsWith("System.IdentityModel."))
+                    if (WilsonAssemblyNames.Contains(assembly.Name, StringComparer.OrdinalIgnoreCase))
                     {
-                        wilsonAssemblies.Add(assembly);
+                        wilsonMessage.Append(assembly.Name + ":" + assembly.Version);
+                        wilsonMessage.Append(";");
+                        foundWilsonAssemblies.Add(assembly);
+                    }
+
+                    if (SalAssemblyNames.Contains(assembly.Name, StringComparer.OrdinalIgnoreCase))
+                    {
+                        salMessage.Append(assembly.Name + ":" + assembly.Version);
+                        salMessage.Append(";");
+                        foundSalAssemblies.Add(assembly);
                     }
                 }
 
-                var wilsonVersion = wilsonAssemblies.Any() ? wilsonAssemblies.First().Version : null;
-                var message = new StringBuilder();
-                if (wilsonVersion != null)
-                {
-                    foreach (var item in wilsonAssemblies)
-                    {
-                        message.Append(item.Name + ":" + item.Version);
-                        message.Append(";");
-                    }
-
-                    if (!wilsonAssemblies.All(x => x.Version == wilsonVersion))
-                    {
-                        ctxt.ReportDiagnostic(
+                if (DetectVersionMismatch(foundWilsonAssemblies))
+                    ctxt.ReportDiagnostic(
                             Diagnostic.Create(
                                 AssembliesVersionMismatch,
                                 null,
                                 compilation.AssemblyName,
-                                message
+                                wilsonMessage
                                 ));
-                    }
-                }
+
+                if (DetectVersionMismatch(foundSalAssemblies))
+                    ctxt.ReportDiagnostic(
+                            Diagnostic.Create(
+                                AssembliesVersionMismatch,
+                                null,
+                                compilation.AssemblyName,
+                                salMessage
+                                ));
 
                 return;
             });
+        }
+
+        private bool DetectVersionMismatch(List<AssemblyIdentity> assemblies)
+        {
+            var version = assemblies.Any() ? assemblies.First().Version : null;
+
+            if (version != null)
+            {
+                if (!assemblies.All(x => x.Version == version))
+                    return true;
+            }
+
+            return false;
         }
     }
 }
